fbd8e5e2ecbc2ef8b53c42b4f8a37e47ecd734c1
Bhavya220298 <94981530+Bhavya220298@users.noreply.github.com>
2021-11-24 21:21:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import json
+
+app =  FastAPI()
+data=[{
+  "name": "Elon Musk",
+  "email": "melwin96@yahoo.com",
+  "image": "https://funkylife.in/wp-content/uploads/2021/10/happy-birthday-wishes-1.jpg"
+},
+{
+  "name": "Jeff Bezos",
+  "email": "sanjayamazed@gmail.com",
+  "image": "https://funkylife.in/wp-content/uploads/2021/10/happy-birthday-wishes-1.jpg"
+}]
+
+@app.get("/")
+def root():
+	return data
+
+@app.get("/{name}")
+def return_data_based_on_name(key: str):
+	for i in data:
+		if i['name'] == key:
+			return i
